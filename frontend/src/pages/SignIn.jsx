1d9dea4e59b4bc51fe4d68c072c9e3da066f1f36
nethuchathuu <155764628+nethuchathuu@users.noreply.github.com>
2025-08-26 20:17:53 +0530

    first commit

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignIn() {
+  return (
+    <div className="text-white text-center mt-10">
+      <h1>Sign In Page</h1>
+    </div>
+  );
+}
+
+export default SignIn;  // <-- Add this line
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+function SignUp() {
+  return (
+    <div className="text-white text-center mt-10">
+      <h1>Sign Up Page</h1>
+    </div>
+  );
+}
+
+export default SignUp;
