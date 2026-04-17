a8736914906324f63d68969635870af55c9d7518
nethuchathuu <155764628+nethuchathuu@users.noreply.github.com>
2026-04-18 01:50:54 +0530

    new

--- Backend/middlewares/verifyUser.js
+++ Backend/middlewares/verifyUser.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET
-
 const verifyUser = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
     return res.status(401).json({ error: 'No token provided' });
   }
   try {
+    const JWT_SECRET = process.env.JWT_SECRET;
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded; // Attach user data (id, role, etc.)
     console.log('User verified:', req.user);
